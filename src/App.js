6d7da85b2d331b594f79c7a56c885f2c963df2bc
Senagoka <gokasena24@gmail.com>
2021-05-24 10:39:08 +0000

    made changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
-import Cart from "./components/carts"; 
+import Cart from "./components/Cart/Cart"; 
 import Default from "./components/Default";
 import Modal from "./components/Modal";
 import AddressForm from "./components/AddressForm";
